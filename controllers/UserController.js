23b83dd5a18427f52a0b08cbd23c62e9f4bf56cf
romiyusnandar <yusromi04@gmail.com>
2025-01-01 20:24:40 +0700

    Add create user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const prisma = require("../prisma/client")
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs")
 
 const findUsers = async (req, res) => {
   try {
@@ -30,4 +32,44 @@ const findUsers = async (req, res) => {
   }
 }
 
-module.exports = { findUsers }
+// create user
+const createUser = async (req, res) => {
+  // periksa validasi
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      success: false,
+      message: "Validation error",
+      errors: errors.array(),
+    })
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+  try {
+    // insert data
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      },
+    })
+    // send response
+    res.status(201).send({
+      success: true,
+      message: "User created successfully",
+      data: user
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      success: false,
+      message: "Server error"
+    })
+  }
+}
+
+module.exports = { findUsers, createUser }
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ const registerController = require("../controllers/RegisterController")
 const loginController = require("../controllers/LoginController")
 const userController = require("../controllers/UserController")
 const { validateRegister, validatorLogin } = require("../utils/validators/auth")
+const validateUser = require('../utils/validators/user');
 
 router.post("/register", validateRegister, registerController.register)
 router.post("/login", validatorLogin, loginController.login)
+
+// user routes
 router.get("/admin/users", verifyToken, userController.findUsers)
+router.post("/admin/users", verifyToken, validateUser, userController.createUser)
 
 module.exports = router
